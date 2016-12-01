a94426d5ef00e87fb8c334897d7b5ec609527d35
maimous@gmail.com <stefaleon@gmail.com>
2016-12-01 14:30:39 +0200

    initial commit

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+	res.send('Ohai!');
+});
+
+app.listen(3000, () => {
+	console.log('server started on port 3000');
+});

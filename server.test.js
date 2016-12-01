47b9f2994f1861d966733042d80349673ea123d1
maimous@gmail.com <stefaleon@gmail.com>
2016-12-01 16:41:09 +0200

    organize with describe

--- server.test.js
+++ server.test.js
@@ -3,53 +3,62 @@ const expect = require('expect');
 
 var app = require('./server').app;
 
-it('should return an object', (done) => {
-	request(app)
-		.get('/')
-		.expect(200)
-		.expect({
-		title: 'Simple Express Server',
-		stack: 'Mocha, expect, supertest'
-	})		
-		.end(done);	
-});
-
-it('should include stack', (done) => {
-	request(app)
-		.get('/')		
-		.expect((res) => {
-			expect(res.body).toInclude({
+describe('Server testing',() => {
+	
+	describe('GET /', () => {
+		it('should return an object', (done) => {
+			request(app)
+				.get('/')
+				.expect(200)
+				.expect({
+				title: 'Simple Express Server',
 				stack: 'Mocha, expect, supertest'
-			});
-		})		
-		.end(done);	
-});
+			})		
+				.end(done);	
+		});
 
-it('should return an array of objects', (done) => {
-	request(app)
-		.get('/users')
-		.expect(200)
-		.expect([{
-		user: 'admin',
-		status: 'administrator'
-	}, {
-		user: 'guest',
-		status: 'guest'
-	}, {
-		user: 'super',
-		status: 'superuser'
-	}])
-		.end(done);
-});
+		it('should include "stack"', (done) => {
+			request(app)
+				.get('/')		
+				.expect((res) => {
+					expect(res.body).toInclude({
+						stack: 'Mocha, expect, supertest'
+					});
+				})		
+				.end(done);	
+		});
+	});	
 
-it('should include user admin', (done) => {
-	request(app)
-		.get('/users')
-		.expect((res) => {
-			expect(res.body).toInclude({
+	describe('GET /users', () => {
+		it('should return an array of objects', (done) => {
+			request(app)
+				.get('/users')
+				.expect(200)
+				.expect([{
 				user: 'admin',
 				status: 'administrator'
-			});
-		})
-		.end(done);
+			}, {
+				user: 'guest',
+				status: 'guest'
+			}, {
+				user: 'super',
+				status: 'superuser'
+			}])
+				.end(done);
+		});
+
+		it('should include user "admin"', (done) => {
+			request(app)
+				.get('/users')
+				.expect((res) => {
+					expect(res.body).toInclude({
+						user: 'admin',
+						status: 'administrator'
+					});
+				})
+				.end(done);
+		});
+	});
+
 });
+
